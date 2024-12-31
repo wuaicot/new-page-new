60832ab1910e9dae9ef956bdd98c3e129fa1022c
Naycol Linares <wuaicot8@gmail.com>
2024-12-31 19:52:40 -0300

    adding background in plataformsection

--- src/components/HomePage/HeroSection.jsx
+++ src/components/HomePage/HeroSection.jsx
@@ -8,7 +8,7 @@ export default function HeroSection() {
       <h1 className="text-5xl text-sky-500 font-bold mb-6 tracking-wide">
         Build Something Meaningful
       </h1>
-      <p className="text-lg font-bold text-blue-950 max-w-3xl mx-4">
+      <p className="text-lg font-bold text-blue-950 max-w-3xl mx-4 my-12">
         The first thing someone asks when you talk about your brand is, 
         “Where can I check you out?”. Whether you’re a business or a self-employed individual, the world deserves to know you. 
         With your vision and our technological consulting, let’s place you on the digital map of success!
--- src/components/HomePage/PlatformSection.jsx
+++ src/components/HomePage/PlatformSection.jsx
@@ -1,14 +1,18 @@
 // File: /app/components/PlatformSection.jsx
 export default function PlatformSection() {
   return (
-    <section className="py-20 bg-gray-900">
+    <section className="flex flex-col items-center justify-center h-screen text-center bg-fixed bg-cover bg-center" 
+    style={{ backgroundImage: "url('/assets/hero-bg.png')",
+      backgroundSize: 'cover',
+     backgroundPosition: 'center',
+     }}>
       <div className="text-center">
-        <h2 className="text-4xl font-bold mb-6">Create Your Own Platform</h2>
-        <p className="max-w-2xl mx-auto">
+        <h2 className="text-5xl text-sky-500 font-bold mb-6 tracking-wide">Create Your Own Platform</h2>
+        <p className="text-lg font-bold text-blue-950 max-w-3xl mx-4 my-28">
           Wish you could have your products and services in one place? 
           With our secure IT solutions, have your own web and mobile application set up in no time.
         </p>
-        <button className="mt-8 px-6 py-3 bg-blue-500 hover:bg-blue-600 text-white rounded-lg text-lg font-medium transition-all">
+        <button className="mt-8 px-6 py-1 mx-4 bg-cyan-700 hover:bg-blue-700 text-white rounded-lg text-lg font-medium transition-all">
           Explore Now – Web Application and Mobile Application
         </button>
       </div>
