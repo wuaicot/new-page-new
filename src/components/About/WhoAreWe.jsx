3c6ce4c356530b332cc658e862772deba8d91a97
Naycol Linares <wuaicot8@gmail.com>
2024-12-31 23:19:34 -0300

    aline on md

--- src/components/About/WhoAreWe.jsx
+++ src/components/About/WhoAreWe.jsx
@@ -9,27 +9,27 @@ const WhoAreWe = () => {
      zIndex: 0,
      
      }}id="who-are-we">
-      <h2 className="md:translate-x-0 md:-translate-y-20 text-5xl text-sky-500 font-bold mt-60 sm:mb-0 tracking-wide">Who Are We?</h2>
-      <p className="md:translate-x-0 lg:translate-x-80  md:-translate-y-20 mr-12 text-lg font-bold text-blue-950 max-w-3xl  text-right mx-4 my-6">
+      <h2 className="md:translate-x-0 md:translate-y-2/5 text-5xl text-sky-500 font-bold mt-60 sm:mb-0 tracking-wide">Who Are We?</h2>
+      <p className="md:translate-x-0 lg:translate-x-80  md:-translate-y-1/4 mr-12 text-md font-bold text-blue-950 max-w-3xl  text-right mx-4 my-6">
         We, at <span className="text-teal-400">S3A Technologies</span>, are a
         group of highly-qualified and innovative individuals. The ‘S’ in our
         name stands for ‘support’, the ‘A’ for ‘achieving’, and 3 for 3x
         efficiency. That’s how we do it at S3A Technologies!
       </p>
-      <p className="lg:translate-x-80 md:translate-x-0 md:-translate-y-20 mr-12 text-right text-lg font-bold text-blue-950 max-w-3xl mx-4 text-justify">
+      <p className="lg:translate-x-80 md:translate-x-0 md:-translate-y-1/4 mr-12 text-right text-md font-bold text-blue-950 max-w-3xl mx-4 text-justify">
         With the expertise of IT wizards, we offer the best-in-class digital
         solutions for you and your company. We’re not just here to provide a
         service, we are here to provide solutions. Understanding the current
         market pressures, analyzing your company’s needs, and delivering the
         desired success to your doorstep.
       </p>
-      <p className="sm:translate-x-full md:translate-x-0 md:-translate-y-20 lg:translate-x-2/4 lg:mr-40  text-right text-lg font-bold text-blue-950 max-w-3xl  my-6 mx-12 text-justify">
+      <p className="sm:translate-x-full md:translate-x-0 md:-translate-y-2/3 lg:translate-x-2/4 lg:mr-40  text-right text-md font-bold text-blue-950 max-w-3xl  my-6 mx-12 text-justify">
         But don’t take our word for it, listen to the experienced Technocrats,
         our esteemed clients, and private businesses that swear by us.
       </p>
       <a
         href="#testimonials"
-        className="mt-2  inline-block px-6 py-3 my-48 bg-teal-400 text-black font-medium rounded hover:bg-teal-500 transition md:translate-x-60 "
+        className="mt-2  inline-block px-6 py-3 my-48 bg-teal-400 text-black font-medium rounded hover:bg-teal-500 transition md:translate-x-3/4 md:-translate-y-14 "
       >
         Learn More -- Testimonials
       </a>
