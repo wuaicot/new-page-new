d588e7573d61ae788a64b953215ab51ca684c000
Naycol Linares <wuaicot8@gmail.com>
2025-01-01 15:58:45 -0300

    centrfocusing brandsectionII

--- src/components/HomePage/BrandSection.jsx
+++ src/components/HomePage/BrandSection.jsx
@@ -10,7 +10,7 @@ export default function BrandSection() {
       <div className="text-center  max-w-3xl mx-auto">
         <h2 className="text-5xl text-sky-500 font-bold sm:mb-20 sm:-mt-20 tracking-wide">Expand Your Brand</h2>
 
-        <p className="text-justify mx-6 text-lg sm:-my-20 font-bold text-blue-950 max-w-3xl">
+        <p className="text-justify mx-6 text-lg sm:-my-10 font-bold text-blue-950 max-w-3xl">
           Your brand is more than just a name. Take it to the next level with creative expertise and technological edge. 
           We give you the ‘X’ factor you are looking for. Why be good when you can be the best?
         </p>       
