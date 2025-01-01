05cfd574241fbdd15bb4ccd262bc287db0dd1d07
Naycol Linares <wuaicot8@gmail.com>
2025-01-01 20:47:52 -0300

    centrfocusing8+

--- src/components/About/VisionMission.jsx
+++ src/components/About/VisionMission.jsx
@@ -1,20 +1,38 @@
-// Location: /app/components/About/VisionMission.jsx
+
+
+
+
+
+
+
+
+
+
+
 import React from "react";
+//import "globals.css";
+import "tailwindcss/tailwind.css";
+
 
 const VisionMission = () => {
   return (
-    <section className="py-16 bg-gray-800 text-white px-8" id="vision-mission">
-      <h2 className="text-4xl font-bold mb-6">Vision & Mission</h2>
+    <section className="flex z-10 flex-col items-center justify-center h-screen text-center bg-fixed bg-cover bg-center"
+    style={{
+      backgroundImage: "url('/assets/rb_177769.png')",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      zIndex: 0,
+      backgroundAttachment: "fixed",
+      backgroundRepeat: "no-repeat",
+    }}>
+      <h2 className="text-4xl font-bold text-indigo-800 mb-2 -mt-4 animate-bounce x">Vision & Mission</h2>
       <div className="mb-8">
-        <h3 className="text-2xl font-semibold text-teal-400">Vision</h3>
-        <p className="text-lg leading-relaxed mt-2">
-          Our vision is to put each one of you on the map of success with the
-          unique power of digital technology.
-        </p>
+        <h3 className="text-2xl font-semibold text-teal-400 animate-pulse">Vision</h3>
+        <p className="text-lg leading-relaxed text-slate-900 tracking-wide text-md text-center text-justify mx-20 mt-2 t"> Our vision is to put each one of you on the map of success with the unique power of digital technology. </p>
       </div>
       <div>
-        <h3 className="text-2xl font-semibold text-teal-400">Mission</h3>
-        <p className="text-lg leading-relaxed mt-2">
+        <h3 className="text-2xl font-semibold text-teal-400 animate-pulse -translate-y-10">Mission</h3>
+        <p className="text-lg leading-relaxed text-slate-900 tracking-wide  text-md text-center text-justify mx-20 -translate-y-8">
           To provide state-of-the-art scalable IT solutions that give you a
           competitive edge in the world of business and technology.
         </p>
--- src/components/About/WhoAreWe.jsx
+++ src/components/About/WhoAreWe.jsx
@@ -2,53 +2,53 @@ import React from "react";
 
 const WhoAreWe = () => {
   return (
-    <section className="flex z-10 flex-col items-center justify-center h-screen text-center bg-fixed bg-cover bg-center" 
-    style={{ backgroundImage: "url('/assets/rb_177769.png')",
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-     zIndex: 0,
-      backgroundAttachment: 'fixed',
-      backgroundRepeat: 'no-repeat',
-     
-
-     
-     }}
-     id="who-are-we">
-      <div className='justify-center items-center flex flex-col'>
-      <h2 className="text-5xl text-center text-sky-500 font-bold mt-40 mb-12 tracking-wide md:-translate-x-1/2 sm:translate-y-8">Who Are We?</h2>
-      <p className=" text-md font-bold text-blue-950 max-w-3xl  text-center mb-4 md:-translate-y-1  text-justify text-center tracking-wide sm:mx-20">  
-        We, at <span className="text-teal-400">S3A Technologies</span>, are a
-        group of highly-qualified and innovative individuals. The ‘S’ in our
-        name stands for ‘support’, the ‘A’ for ‘achieving’, and 3 for 3x
-        efficiency. That’s how we do it at S3A Technologies!
-      </p>
-      <p className="sm:mx-20 text-md font-bold text-blue-950 max-w-3xl  text-center mb-12  text-justify text-center tracking-wide
+    <section
+      className="flex z-10 flex-col items-center justify-center h-screen text-center bg-fixed bg-cover bg-center"
+      style={{
+        backgroundImage: "url('/assets/rb_177769.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        zIndex: 0,
+        backgroundAttachment: "fixed",
+        backgroundRepeat: "no-repeat",
+      }}
+      id="who-are-we"
+    >
+      <div className="justify-center items-center flex flex-col">
+        <h2 className="text-5xl text-center text-indigo-800 font-bold mt-40 mb-12 tracking-wide md:-translate-x-1/2 sm:translate-y-8">
+          Who Are We?
+        </h2>
+        <p className=" text-md font-bold text-blue-950 max-w-3xl  text-center mb-4 md:-translate-y-1  text-justify text-center tracking-wide sm:mx-20">
+          We, at <span className="text-teal-400">S3A Technologies</span>, are a
+          group of highly-qualified and innovative individuals. The ‘S’ in our
+          name stands for ‘support’, the ‘A’ for ‘achieving’, and 3 for 3x
+          efficiency. That’s how we do it at S3A Technologies!
+        </p>
+        <p
+          className="sm:mx-20 text-md font-bold text-blue-950 max-w-3xl  text-center mb-12  text-justify text-center tracking-wide
       text-center
-      mb-4 md:translate-y-0 md:-translate-y-3">
-        With the expertise of IT wizards, we offer the best-in-class digital
-        solutions for you and your company. We’re not just here to provide a
-        service, we are here to provide solutions. Understanding the current
-        market pressures, analyzing your company’s needs, and delivering the
-        desired success to your doorstep.
-      </p>
-      <p className="sm:mx-20 tracking-wide  text-md font-bold text-blue-950 max-w-3xl  text-center mb-4  text-justify text-center sm:-translate-y-12 mb-4 md:-translate-y-4">
-        But don’t take our word for it, listen to the experienced Technocrats,
-        our esteemed clients, and private businesses that swear by us.
-      </p>
-      <a
-        href="#testimonials"
-        className="animate-pulce   inline-block  px-6 py-1  bg-teal-400 text-black font-medium rounded hover:bg-teal-500 transition-full 
+      mb-4 md:translate-y-0 md:-translate-y-3"
+        >
+          With the expertise of IT wizards, we offer the best-in-class digital
+          solutions for you and your company. We’re not just here to provide a
+          service, we are here to provide solutions. Understanding the current
+          market pressures, analyzing your company’s needs, and delivering the
+          desired success to your doorstep.
+        </p>
+        <p className="sm:mx-20 tracking-wide  text-md font-bold text-blue-950 max-w-3xl  text-center mb-4  text-justify text-center sm:-translate-y-12 mb-4 md:-translate-y-4">
+          But don’t take our word for it, listen to the experienced Technocrats,
+          our esteemed clients, and private businesses that swear by us.
+        </p>
+        <a
+          href="#testimonials"
+          className="animate-pulce   inline-block  px-6 py-1  bg-teal-400 text-indigo-800 font-medium rounded hover:bg-teal-500 transition-full 
          sm:mb-40 
         md:translate-x-3/4 md:-translate-y-80     
          sm:mb-30  sm:mt-0 sm:mb-30 "
-         
-         
-      >
-        Learn More -- Testimonials
-      </a>
+        >
+          Learn More → Testimonials
+        </a>
       </div>
-
-      
     </section>
   );
 };
