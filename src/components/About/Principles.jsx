298782f8ba8b269232779e179a18390d98e8b667
Naycol Linares <wuaicot8@gmail.com>
2025-01-01 21:19:12 -0300

    centrfocusingfin

--- src/components/About/Principles.jsx
+++ src/components/About/Principles.jsx
@@ -2,11 +2,22 @@ import React from "react";
 
 const Principles = () => {
   return (
-    <section className="py-12 bg-gray-900 text-white px-4 sm:px-6 lg:px-12" id="principles">
-      <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold mb-4 text-center">
+    <section
+      className="flex z-10 flex-col items-center justify-center h-screen text-center bg-fixed bg-cover bg-center px-4 sm:px-6 lg:px-12"
+      style={{
+        backgroundImage: "url('/assets/rb_177769.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        zIndex: 0,
+        backgroundAttachment: "fixed",
+        backgroundRepeat: "no-repeat",
+      }}
+      id="principles"
+    >
+      <h2 className="text-xl sm:text-2xl lg:text-3xl font-bold mb-3 text-center text-indigo-800 -mt-4">
         Our Principles
       </h2>
-      <ul className="list-disc list-inside space-y-3 text-sm sm:text-base lg:text-lg">
+      <ul className="list-disc list-inside space-y-2 text-xs sm:text-sm lg:text-base text-slate-900">
         <li>
           <strong>Observe:</strong> Your needs and wants.
         </li>
@@ -20,7 +31,7 @@ const Principles = () => {
           <strong>Shine:</strong> In this competitive world of marketing.
         </li>
       </ul>
-      <p className="mt-4 text-sm sm:text-base lg:text-lg leading-relaxed text-justify">
+      <p className="mt-4 text-xs sm:text-sm lg:text-base leading-relaxed text-slate-900 text-justify md:mx-20">
         Observe, Create, Shape, and Shine — these principles drive us. What we
         do at S3A Technologies is guided by these pillars.
       </p>
